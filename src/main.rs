0521404406d53718fc05517f7bb394d8cff03fe2
Evan Lundell <elundell@kipsu.com>
2024-12-09 14:34:32 -0600

    day9

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
-mod day8;
+mod day9;
 
 fn main() {
-    let contents = fs::read_to_string("src/input/day8.txt")
+    let contents = fs::read_to_string("src/input/day9.txt")
         .expect("Something went wrong reading the file");
-    println!("Day 8 Part 1: {}", day8::part1(&contents));
-    println!("Day 8 Part 2: {}", day8::part2(&contents));
+    println!("Day 9 Part 1: {}", day9::part1(&contents));
+    println!("Day 9 Part 2: {}", day9::part2(&contents));
 }
